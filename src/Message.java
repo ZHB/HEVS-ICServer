c65d38ed9a547763a436617105a2119fc28a0d94
Vincent <vincent.huck.pro@gmail.com>
2014-04-29 20:39:12 +0200

    Test classes de base

---
+++ src/Message.java
@@ -0,0 +1,29 @@
+import java.util.Date;
+
+
+public class Message {
+
+	private String message;
+	private Date date;
+	
+	public Message(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
